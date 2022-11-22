4d4a79c2f101a33768bd9b952b394d26e37e430a
amn <satija.aman@gmail.com>
2022-11-23 01:00:46 +0530

    loadtest mapstats : removed unwanted code

--- src/utils/__test__/load__test/ltdms.js
+++ src/utils/__test__/load__test/ltdms.js
@@ -54,34 +54,7 @@ let baseUrl =     "https://tnapi.unisolve.org/api/v1"
 export default function () {
   group('simple mentor journey', (_) => {
 
-  //   let id = new Date().getTime();
-  //   id = ""+ id+ Math.random();
-  //   let bodyRegister = JSON.stringify({
-  //   username: 'prefUser' + id  + '@unisolve.org',
-  //   "full_name": "mentor user",
-  //   "password": "112233",
-  //   "mobile": "961911898"+ id,
-  //   "role": "MENTOR",
-  //   "team_id": 1,
-  //   "date_of_birth": "1989-06-20",
-  //   "organization_code": "33320501503",
-  //   "qualification": "Degree",
-  //   "city": "testingCity",
-  //   "district": "testingDistrict",
-  //   "state": "testState",
-  //   "country": "testCountry"
-  // });
   
-  // let bodySubmitResponse = JSON.stringify({
-  //   "quiz_question_id":1,
-  //   "selected_option":"Problem Maker"
-  // });
-  
-  // let bodyLogin = JSON.stringify({
-  //   username: 'prefUser' + id + '@unisolve.org',
-  //   password: 'wHm6eGCL7uFOArs='
-  // });
-
   const params = {
     headers: {
       'Content-Type': 'application/json'
@@ -91,111 +64,15 @@ export default function () {
     }
   };
 
-    
-    // mentor register
-    // console.log(bodyRegister)
-    // const register_response = http.post(baseUrl+'/mentors/register', bodyRegister, params);
-    // check(register_response, {
-    //   'is status register 201': (r) => r.status === 201
-    // });
-    // console.log(getTimeStamp()+" register_response",register_response.json())
-    // // console.log(getTimeStamp()+" register_response_timing",register_response.timings)
-    // const user_id = register_response.json()["data"][0]['user_id'];
-    // console.log("user_id",user_id)
-    // sleep(SLEEP_DURATION);
-    
-    // // mentor verify 
-    // params.tags.name = 'validateOtp';
-    // let bodyValidateOtp = JSON.stringify({
-    //   user_id: user_id,
-    //   otp: '112233'
-    // });
-    // console.log(getTimeStamp()+" bodyValidateOtp",bodyValidateOtp);
-    // const verify_response = http.post(baseUrl+'/mentors/validateOtp', bodyValidateOtp, params);
-    // check(verify_response, {
-    //   'is status otp 200': (r) => r.status === 200,
-    //   // 'is user_id key preset': (r) => r.json().hasOwnProperty('data'),
-    // });
-    // console.log(getTimeStamp()+" verify_response",verify_response.json())
-    // // console.log(getTimeStamp()+" verify_response_timing",verify_response.timings)
-    // sleep(SLEEP_DURATION);
-
-    // mentor update password
-    // params.tags.name = 'passwordUpdate';
-    // let bodyPassword = JSON.stringify({
-    //   user_id: user_id,
-    //   old_password: '112233',
-    //   new_password: 'wHm6eGCL7uFOArs='
-    // });
-    // console.log(getTimeStamp()+" bodyPassword",bodyPassword);
-    // const password_update_response = http.put(baseUrl+'/mentors/updatePassword', bodyPassword, params);
-    // check(password_update_response, {
-    //   'is status password 202': (r) => r.status === 202
-    // });
-    // console.log(getTimeStamp()+" password_update_response",password_update_response.json());
-    // // console.log(getTimeStamp()+" password_update_response_timings",password_update_response.timings);
-    // // console.log("password_update_response",password_update_response.json())
-    // sleep(SLEEP_DURATION);
-
-    // mentor login
-    // params.tags.name = 'login';
-    // const login_response = http.post(baseUrl+'/mentors/login', bodyLogin, params);
-    // console.log(getTimeStamp()+" bodyLogin",bodyLogin)
-    // check(login_response, {
-    //   'is status login 200': (r) => r.status === 200,
-    //   // 'is api key present': (r) => r.json().hasOwnProperty('data'),
-    // });
-    
-    // console.log(getTimeStamp()+" login_response",login_response.json())
-    // // console.log(getTimeStamp()+" login_response_timing",login_response.timings)
-    // if(login_response && 
-    //    login_response.json()&&
-    //    login_response.json()['data'] && 
-    //    login_response.json()['data'][0]&& 
-    //    login_response.json()['data'][0]['token']){
-
+  //dashboardMapStats
+  params.tags.name = 'dashboardMapStats';
+  const get_dashboardMapStats = http.get(baseUrl+'/dashboard/mapStats', params);
+  check(get_dashboardMapStats, {
+    'is status get_dashboardMapStats 200': (r) => r.status === 200
+  });
+  console.log("get_dashboardMapStats",get_dashboardMapStats.json())
+  sleep(SLEEP_DURATION);
       
-    //   params.headers['Authorization'] = 'Bearer ' + login_response.json()['data'][0]['token'];
-    //   // console.log("Authorization",params.headers['Authorization'] )
-    //   sleep(SLEEP_DURATION);
-    //   //quiz_survey
-    //   params.tags.name = 'quizSurvey';
-    //   const get_quiz_survey_response = http.get(baseUrl+'/quizSurveys/1', params);
-    //   check(get_quiz_survey_response, {
-    //     'is status quizSurvey 200': (r) => r.status === 200
-    //   });
-
-    //   //quiz_survey
-    //   params.tags.name = 'quiNeextQuestion';
-    //   const get_quiz_nxtQsn_response = http.get(baseUrl+'/quiz/1/nextQuestion', params);
-    //   check(get_quiz_nxtQsn_response, {
-    //     'is status nxtQsnt 200': (r) => r.status === 200
-    //   });
-    //   console.log("get_quiz_nxtQstn_response",get_quiz_nxtQsn_response.json())
-    //   sleep(SLEEP_DURATION);
-    //   //quiz_survey
-    //   params.tags.name = 'quizSubmitResponse';
-    //   const post_quiz_response = http.post(baseUrl+'/quiz/1/response',bodySubmitResponse, params);
-    //   check(post_quiz_response, {
-    //     'is status quizResponse 200': (r) => r.status === 200
-    //   });
-    //   console.log("post_quiz_response",post_quiz_response.json())
-
-
-      //dashboardMapStats
-      params.tags.name = 'dashboardMapStats';
-      const get_dashboardMapStats = http.get(baseUrl+'/dashboard/mapStats', params);
-      check(get_dashboardMapStats, {
-        'is status get_dashboardMapStats 200': (r) => r.status === 200
-      });
-      console.log("get_dashboardMapStats",get_dashboardMapStats.json())
-      sleep(SLEEP_DURATION);
-      // console.log("get_quiz_survey_response",get_quiz_survey_response.json())
-      // console.log(getTimeStamp()+" get_quiz_survey_response",get_quiz_survey_response.timings)
-      // sleep(SLEEP_DURATION);
-    // }else{
-    //   console.log(getTimeStamp()+" login failed : response ==",login_response);
-    // }
 
   });
 }
