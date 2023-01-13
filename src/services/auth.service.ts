a612a7cd74fcf7cc7488efd1b5d3608ac91f0479
vamshiInqui-lab <97531883+vamshiInqui-lab@users.noreply.github.com>
2023-01-13 22:57:53 +0530

    Update auth.service.ts

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -84,18 +84,18 @@ export default class authService {
             if (user_data) {
                 throw badRequest('Email');
             } else {
-                const mentor_data = await this.crudService.findOne(mentor, { where: { mobile: requestBody.mobile } })
-                if (mentor_data) {
-                    throw badRequest('Mobile')
-                } else {
-                    let createUserAccount = await this.crudService.create(user, requestBody);
-                    let conditions = { ...requestBody, user_id: createUserAccount.dataValues.user_id };
-                    let createMentorAccount = await this.crudService.create(mentor, conditions);
-                    createMentorAccount.dataValues['username'] = createUserAccount.dataValues.username;
-                    createMentorAccount.dataValues['user_id'] = createUserAccount.dataValues.user_id;
-                    response = createMentorAccount;
-                    return response;
-                }
+                // const mentor_data = await this.crudService.findOne(mentor, { where: { mobile: requestBody.mobile } })
+                // if (mentor_data) {
+                //     throw badRequest('Mobile')
+                // } else {
+                let createUserAccount = await this.crudService.create(user, requestBody);
+                let conditions = { ...requestBody, user_id: createUserAccount.dataValues.user_id };
+                let createMentorAccount = await this.crudService.create(mentor, conditions);
+                createMentorAccount.dataValues['username'] = createUserAccount.dataValues.username;
+                createMentorAccount.dataValues['user_id'] = createUserAccount.dataValues.user_id;
+                response = createMentorAccount;
+                return response;
+                // }
             }
         } catch (error) {
             return error;
