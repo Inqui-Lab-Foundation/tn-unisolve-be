1b1c7881a11f048f582133d9cac702765e8718f7
vamshiInqui-lab <97531883+vamshiInqui-lab@users.noreply.github.com>
2023-01-31 16:42:11 +0530

    Jobs

--- src/jobs/badges.jobs.ts
+++ src/jobs/badges.jobs.ts
@@ -3,14 +3,14 @@ import BaseJobs from "./base.job";
 
 export default class BadgesJob extends BaseJobs {
 
-
+    // initialize job
     protected init() {
         this.name = 'BadgesJob';
         this.period = "* * * * * *"
     };
 
+    // invoking the execute job
     public async executeJob() {
         super.executeJob();
-        //TODO: write the logic to execute to badges Job...!!
     }
 }
